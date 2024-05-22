8d10ecae460ea7d3d98fbb075c84fd750aeed4ef
prabhpreetS <prabhpreetsingh1707@gmail.com>
2024-05-22 17:26:07 +0530

    sec commit

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 import os
-openai_key="sk-proj-TVsSL9qX855h6dZMBuKLT3BlbkFJIfi776I9mT2Bo3egEnT1"
+openai_key=""
 OPENAI_API_KEY=openai_key
-langchain_api_key="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"
+langchain_api_key=""
